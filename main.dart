8ad9e02cd6fac7a5cd35332a79dd118375ed26a4
Ali Alsarraf <ali.alsarraf@csu.fullerton.edu>
2024-09-30 11:37:05 +0300

    final

---
+++ main.dart
@@ -0,0 +1,33 @@
+void main(){
+  String firstName = "Ali";
+  String lastName = "Alsarraf";
+  int age = 22;
+  double height = 1.73;
+  bool married = false;
+  double temperature = 20;
+  String drink = 'juice';
+  String flavor = 'orange';
+  int number = 5;
+  int result;
+  String fullName = "Ali Alsarraf";
+
+  print("The temperature is $temperature C");
+  print("I like $flavor $drink");
+
+  result = number + number;
+  print("$number + $number = $result");
+
+  print("My name is $firstName and my last name length is ${lastName.length}");
+
+  print("Does My Last Name Starts With The Letter d? ${lastName.contains("d")}");
+
+
+
+
+
+
+
+
+
+
+}
